d7690d4caf96d2475b6cc9727925fe15c5c0b148
SpectreVert <bushnaq.costa@gmail.com>
2020-02-25 16:23:58 +0100

    [add] Http::Headers class, using newly [add] Umap class for unordered map shenanigans

--- include/og/network/Http/Headers.hpp
+++ include/og/network/Http/Headers.hpp
@@ -9,42 +9,34 @@
 
 #pragma once
 
-#include <map>
+#include "og/base/Umap.hpp"
+
 #include <string>
 
 namespace og {
 
 namespace Http {
 
-/*
-	As per RFC2616 describing HTTP1.1 there can be more than
-	one HTTP header with same name. This implementation offers
-	only a single name-value pair.
-
-	TODO: Implement aforementioned behaviour.
-
-	See: https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html
-*/
-
 class Headers {
 public:
-	typedef std::map<std::string, std::string> HeaderMap;
+	typedef Umap<std::string, std::string> HeadersUmap;
 	
 	Headers();
-	Headers(HeaderMap t_map);
+	Headers(HeadersUmap t_headers);
 
-	const std::string& operator[] (const std::string& t_name);
+	const std::string& operator[] (const std::string& t_name) const;
+	std::string& operator[] (const std::string& t_name);
 
 	bool has(const std::string& t_name) const;
-	void add(const std::string& t_name, const std::string& t_value);
+	void insert(const std::string& t_name, const std::string& t_value);
 	void erase(const std::string& t_name);
 	void clear();
 
 private:
-	HeaderMap m_headers;
+	HeadersUmap m_headers;
 
 };
 
-} /// namespace Http
+} // namespace Http
 
 } // namespace og
--- src/og/network/Http/Headers.cxx
+++ src/og/network/Http/Headers.cxx
@@ -13,3 +13,33 @@ using namespace og::Http;
 
 Headers::Headers()
 {}
+
+Headers::Headers(HeadersUmap t_headers)
+{
+	m_headers = t_headers;
+}
+
+bool Headers::has(const std::string& t_name) const
+{
+	auto it = m_headers.find(t_name);
+
+	if (it != m_headers.end())
+		return true;
+	
+	return false;
+}
+
+void Headers::insert(const std::string& t_name, const std::string& t_value)
+{
+	m_headers.insert(HeadersUmap::ValueType(t_name, t_value));
+}
+
+const std::string& Headers::operator[] (const std::string& t_name) const
+{
+	return m_headers[t_name];
+}
+
+std::string& Headers::operator[] (const std::string& t_name)
+{
+	return m_headers[t_name];
+}

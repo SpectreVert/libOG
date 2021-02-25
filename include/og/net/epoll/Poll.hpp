3c54b764dfec36346dd19e70ee13cae3220ef3f7
SpectreVert <bushnaq.costa@gmail.com>
2021-02-25 11:49:55 +0100

    :art: improve Poll.hpp

--- include/og/net/epoll/Poll.hpp
+++ include/og/net/epoll/Poll.hpp
@@ -19,14 +19,16 @@ namespace net {
 
 //! \brief Poll implementation for epoll
 //!
-class Poll : IPoll<SocketHandle, Events> {
+class Poll : public IPoll<SocketHandle, Events> {
+	int m_epoll_fd;
+
 public:
 	//! \brief Initialize the internal resource for event
 	//!        polling.
 	//!
 	//! \see   is_valid
 	//!
-	Poll();
+	Poll(); // need an assert here instead of using is_valid
     virtual ~Poll();
 
 	//! \brief Indicates if the object has been properly
@@ -37,10 +39,6 @@ public:
     virtual int poll(Events& events, int timeout);
     virtual int add(SocketHandle source, core::Tag id, core::Concern concern);
     virtual int remove(SocketHandle source);
-
-private:
-    int m_epoll_fd;
-
 }; // class Poll
 
 } // namespace net

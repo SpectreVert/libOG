3f0d32a3e4bdb33d1cf5f206cc6493b5a858d377
SpectreVert <bushnaq.costa@gmail.com>
2021-03-09 00:51:56 +0100

    :sparkles: add RawFd

---
+++ include/og/core/RawFd.hpp
@@ -0,0 +1,24 @@
+/*
+ * Created by Costa Bushnaq
+ *
+ * 08-03-2021 @ 22:17:14
+ *
+ * see LICENSE
+*/
+
+#ifndef _RAWFD_HPP_
+#define _RAWFD_HPP_
+
+namespace og {
+
+namespace core {
+
+//! \brief A wrapper around the unix file descriptor type.
+//!
+typedef int RawFd;
+
+} // namespace core
+
+} // namespace og
+
+#endif /* _RAWFD_HPP_ */

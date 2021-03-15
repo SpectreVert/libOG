c627eaa3007649517e3c8a0444df3bc367cb1150
SpectreVert <bushnaq.costa@gmail.com>
2021-03-15 09:19:45 +0100

    :recycle: clean sources

--- src/og/async/Driver.cc
+++ src/og/async/Driver.cc
@@ -5,27 +5,3 @@
 */
 
 #include "og/async/Driver.hpp"
-
-using namespace og::async;
-
-int Driver::dispatch(core::Tag id, net::Events& events)
-{
-	(void) id;
-	(void) events;
-
-	return 0;
-}
-
-int Driver::monitor(net::SocketHandle source, core::Tag id, core::Concern concern)
-{
-	(void) source;
-	(void) id;
-	(void) concern;
-
-	return 0;
-}
-
-int Driver::step(int timeout)
-{
-	return timeout;
-}

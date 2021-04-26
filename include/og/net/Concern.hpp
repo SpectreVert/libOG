c96a065957c50bb954fb62e58409c6cf83b535cb
SpectreVert <bushnaq.costa@gmail.com>
2021-04-26 23:14:33 +0200

    :sparkles: add new Concept

---
+++ include/og/net/Concern.hpp
@@ -0,0 +1,29 @@
+/*
+ * Created by Costa Bushnaq
+ *
+ * 26-04-2021 @ 21:58:10
+ *
+ * see LICENSE
+*/
+
+#ifndef _FIXME
+#define _FIXME
+
+#include <bitset>
+
+namespace og {
+
+namespace net {
+
+enum Concern {
+	e_read,
+	e_write,
+	e_shared
+};
+
+} // namespace net
+
+} // namespace og
+
+#endif /* _FIXME */
+

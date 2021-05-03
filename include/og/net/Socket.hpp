e2d29cad7da65b9c9ac0fff12c27b6310f7d2469
SpectreVert <bushnaq.costa@gmail.com>
2021-05-03 02:23:15 +0200

    :sparkles: #3

---
+++ include/og/net/Socket.hpp
@@ -0,0 +1,45 @@
+/*
+ * Created by Costa Bushnaq
+ *
+ * 27-04-2021 @ 02:30:20
+ *
+ * see LICENSE
+*/
+
+#ifndef _SOCKET_HPP
+#define _SOCKET_HPP
+
+#include "og/core/RawFd.hpp"
+#include "og/net/generic/ISource.hpp"
+#include "og/net/Internal.hpp"
+
+namespace og {
+
+namespace net {
+
+class Socket : public ISource<core::RawFd> {
+public:
+	using Handle = ISource::Handle;
+
+	virtual ~Socket() = default;
+	Socket() = default;
+	Socket(int dom, int type, int prot);
+	Socket(Handle);
+
+	virtual Handle handle() const;
+	virtual void set_handle(Handle);
+	virtual int make_handle(int dom, int type, int prot);
+	virtual int close();
+
+	virtual int bind(SocketAddr const& addr);
+
+protected:
+	Handle m_handle = k_bad_socket;
+
+}; // class Socket
+
+} // namespace core
+
+} // namespace og
+
+#endif /* _SOCKET_HPP */
---
+++ src/og/net/Socket.cc
@@ -0,0 +1,57 @@
+/*
+ * Created by Costa Bushnaq
+ *
+ * 28-04-2021 @ 22:10:15
+*/
+
+#include "og/net/Error.hpp"
+#include "og/net/Socket.hpp"
+
+using namespace og::net;
+
+Socket::Socket(int dom, int type, int prot)
+{
+	(void) make_handle(dom, type, prot);
+}
+
+Socket::Socket(Handle handle)
+	: m_handle(handle)
+{	
+}
+
+Socket::Handle Socket::handle() const
+{
+	return m_handle;
+}
+
+void Socket::set_handle(Handle handle)
+{
+	m_handle = handle;
+}
+
+int Socket::make_handle(int dom, int type, int prot)
+{
+	m_handle = intl::open(dom, type, prot);
+	
+	if (m_handle != k_bad_socket)
+		return e_success;
+
+	return e_failure;
+}
+
+/* not calling close twice is left
+ * to the discretion of the user. */
+int Socket::close()
+{
+	if (intl::close(m_handle) == -1)
+		return e_failure;
+
+	m_handle = k_bad_socket;
+
+	return e_success;
+}
+
+int Socket::bind(SocketAddr const& addr)
+{
+	return intl::bind(m_handle, addr);
+}

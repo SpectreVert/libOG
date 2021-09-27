2ca782f07d404c891f86154977b62382f74a9a72
SpectreVert <bushnaq.costa@gmail.com>
2021-09-27 20:02:20 +0200

    :bug: fix some include

--- og/Poll.hpp
+++ og/Poll.hpp
@@ -7,12 +7,9 @@
 
 #include "og/System.hpp"
 
-// TODO compile the appropriate files from Makefile
-// rather than doing this dirty hack
-
 #if defined(OG_SYSTEM_LINUX) || defined(OG_SYSTEM_ANDROID)
-#  include "og/epoll/Poll.hpp"
-#  include "og/poll/Event.hpp"
+#include "og/epoll/Poll.hpp"
+#include "og/epoll/Event.hpp"
 #else
-#  warning "Your distribution does not support a Poll implementation"
+#warning "Your distribution does not support a Poll implementation"
 #endif
